99c63095212257e4279e6b072849a8f0114317d6
Travis Swicegood <development@domain51.com>
2009-05-23 16:59:46 -0500

    make sure headers get sent

--- src/argil/http/ResourceNotFoundResponse.php
+++ src/argil/http/ResourceNotFoundResponse.php
@@ -4,7 +4,21 @@ namespace argil\http;
 
 class ResourceNotFoundResponse implements \argil\http\ResponseInterface
 {
+    private $_header_callback = 'header';
+    public $headers = array();
+
+    public function __construct($options = array()) {
+        if (isset($options['header_callback'])) {
+            $this->_header_callback = $options['header_callback'];
+        }
+    }
+
     public function __toString() {
+        foreach ($this->headers as $key => $value) {
+            $header = $this->_header_callback;
+            $header("{$key}: {$value}\r\n");
+        }
+        return '';
     }
 }
 
---
+++ tests/http/ResourceNotFoundResponse/send-headers-on-toString.phpt
@@ -0,0 +1,27 @@
+--TEST--
+Should send headers when object is cast to a string
+--FILE--
+<?php
+
+require __DIR__ . '/_config.php';
+
+$accumulator = '';
+
+$response = new \argil\http\ResourceNotFoundResponse(array(
+    'header_callback' => function($header) use (&$accumulator) {
+        $accumulator .= $header;
+    }
+));
+
+$header = "Hello World! " . rand(1, 1000);
+$response->headers['X-SimpleHeader'] = $header;
+(string)$response;
+
+$expected = "X-SimpleHeader: {$header}\r\n";
+assert('$accumulator == $expected');
+
+?>
+===DONE===
+--EXPECT--
+===DONE===
+
