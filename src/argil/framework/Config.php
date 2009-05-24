1ce66a0270d24c6467589fea2c840bbb919a1740
Travis Swicegood <development@domain51.com>
2009-05-23 22:38:08 -0500

    Simple code cleanup

--- src/argil/framework/Config.php
+++ src/argil/framework/Config.php
@@ -7,12 +7,14 @@ class Config
     public $request_method = '';
 
     public function __construct($config = array()) {
-        $this->request_uri = isset($config['REQUEST_URI']) ?
-            $config['REQUEST_URI'] :
-            $_SERVER['REQUEST_URI'];
-        $this->request_method = isset($config['REQUEST_METHOD']) ?
-            $config['REQUEST_METHOD'] :
-            $_SERVER['REQUEST_METHOD'];
+        $values_to_check = array(
+            'REQUEST_URI' => 'request_uri',
+            'REQUEST_METHOD' => 'request_method',
+        );
+
+        foreach ($values_to_check as $key => $instance_key) {
+            $this->$instance_key = isset($config[$key]) ? $config[$key] : $_SERVER[$key];
+        }
     }
 }
 
